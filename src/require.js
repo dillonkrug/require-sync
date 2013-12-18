de2ef2fb05eabd6971ba069325444d0a85fe43cc
dillonkrug <dpkrug@gmail.com>
2013-12-18 17:02:47 -0500

    misc

--- src/require.js
+++ src/require.js
@@ -2,18 +2,18 @@
 	var require = global.require = function(oScope, r) {
 		return r = function(fileString) {
 			if (oScope.err) return;
+
+			if (require._config.globals && require._config.globals[fileString]) fileString = require._config.globals[fileString];
+
+			// console.log(fileString);
 			var iScope = {
-				pointer: fileString.split('/'),
-				i: 0
+				file: fileString.split("/")
 			};
-			for (; iScope.i < iScope.pointer.length; iScope.i++) {
-				if (iScope.pointer[iScope.i] == '..' && iScope.pointer[iScope.i - 1] && iScope.pointer[iScope.i - 1] != '..') {
-					iScope.pointer.splice(iScope.i - 1, 2);
-					iScope.i = iScope.i - 2;
-				}
-			}
-			var __filename = iScope.pointer.join('/') + '.js';
-			var __dirname = iScope.pointer.pop() && iScope.pointer.join('/');
+			// console.log(iScope.file);
+			
+			var __filename = iScope.file.join("/") + '.js';
+			// console.log("__filename",__filename)
+			var __dirname = iScope.file.pop() && iScope.file.join("/");
 
 			if (oScope.loadedJS[__filename]) return oScope.loadedJS[__filename];
 
@@ -26,12 +26,17 @@
 			iScope.requestObj.send();
 			
 			if (iScope.requestObj.status == 404 || !(iScope.loadedCode = iScope.requestObj.responseText)) throw new Error(oScope.errorMsg + __filename + '> Not Found');
+			var subrequire = function(f){
+				return require(require.processURI(__dirname,f));
+			};
 			try {
-				eval('require.loadedJS["' + __filename + '"]=' + (iScope.wrappedCode = '(function(module,require,__filename,__dirname,iScope,oScope,r,fileString){var exports=module.exports;//@ sourceURL=' + __filename + '\n' + iScope.loadedCode + '\n;return module.exports;/* END REQUIRED FILE */})') + '({exports:{}},function(f){return require("' + __dirname + '/"+f)},__filename,__dirname);');
+				eval('require.loadedJS["' + __filename + '"]=' + (iScope.wrappedCode = '(function(module,require,__filename,__dirname,iScope,oScope,r,fileString,subrequire){var exports=module.exports;//@ sourceURL=' + __filename + '\n' + iScope.loadedCode + '\n;return module.exports;/* END REQUIRED FILE */})') + '({exports:{}},subrequire,__filename,__dirname);');
 			} catch (e) {
 				oScope.err = true;
+				// console.log("EVAL ERROR", __filename);
 				try {
 					esprima.parse(iScope.loadedCode);
+					// console.log("ESPRIMA PASSED", __filename);
 				} catch (er) {
 					console.error(oScope.errorMsg + __filename + '> Line ' + er.lineNumber + ' Column ' + er.column + ' -> ' + er.description);
 					throw er;
@@ -50,8 +55,25 @@
 		loadedText: [],
 		loadedJS: {},
 		err: false,
-		errorMsg: 'REQUIRE ERROR: <'
+		errorMsg: 'REQUIRE ERROR: <',
 	});
+	require._config = {}
+
+	require.processURI = function(d, f) {
+		if (/^(https?:)?\/\//.test(f)) return f;
+		// console.log("GLOBALS", require._config.globals)
+		if (require._config.globals && require._config.globals[f]) return require._config.globals[f];
+		if (/^\//.test(f)) return f;
+		var p = (d+"/"+f).split('/');
+		for (var i = 0; i < p.length; i++) {
+			if (p[i] == '..' && p[i - 1] && p[i - 1] != '..') {
+				p.splice(i - 1, 2);
+				i = i - 2;
+			}
+			if (p[i] == '.') p.splice(i,1);
+		}
+		return p.join('/');
+	};
 
 	require.save = function() {
 		var outCode = [],
@@ -64,6 +86,8 @@
 		a.href = window.URL.createObjectURL(outCode);
 		a.click();
 	};
+
+	require.config = function(p) { require._config = p; };
 
 	require.load = function(mods) {
 		var init = function(loadedMod) {
